7bb1b47140940d02c916d39a2f5b09b259faa670
Md.Ashraful Alam <101570492+ashraful-commits@users.noreply.github.com>
2022-03-31 08:35:59 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,102 @@
+// ............Create a loop that will rotate from 1000 to 300
+
+
+for(let Num=1000; Num>=300; Num--){
+    console.log(Num);
+}
+
+
+
+
+
+
+
+
+//........... Find the even and odd numbers from 1 and 1000 through for loop
+
+
+
+// for(let i=1; i<=1000;i++){
+
+//     (i%2==0) ? console.log(`even number ${i}`) : console.log(`Odd number ${i}`)
+// }
+
+
+
+
+
+
+
+
+// .................Find the number divisible by 7 between 1 and 1000
+
+
+
+
+// for(let i=1; i<=1000;i++){
+
+//     (i%7==0) ? console.log(`${i} is Divisible by 7  `) : ""
+// }
+
+
+
+
+
+
+
+
+
+
+
+//.............Create a loop using “for” that will increment by 3 per step and break the loop when it finds a number divisible by 11
+
+
+// for(let i=1; i<=1000; i=i+3){
+//     console.log(i);
+
+//     if(i%11==0){
+//         break;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+//............... Create a loop that rotates 500 times using the for loop where you just pull out the numbers divisible by 3 and 4
+
+
+
+// for(let i=1; i<=100; i++){
+//     (i%3==0 && i%4==0) ? console.log(i) : ""; 
+// }
+
+
+
+
+
+
+// ............Create a loop where the initial value is 10,000. You need to rotate the loop 50 times by using the increment  operator twice.
+
+
+
+
+// for(let i=10000; i<=10050; i++){
+//    console.log(i); 
+// }
+
+
+
+
+
+// Create a loop where the initial value is 10,000. You need to rotate the loop 50 times by using the  decrement operator twice.
+
+
+// for(let i=10000; i>=9950; i--){
+//     console.log(i); 
+//  }
